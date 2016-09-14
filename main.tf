d6a196fabfe979f3394993ddb782e26404123d62
ryane <ryanesc@gmail.com>
2016-09-14 11:05:02 -0400

    remove unused ingress rule

--- main.tf
+++ main.tf
@@ -61,13 +61,6 @@ resource "aws_security_group" "default" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
